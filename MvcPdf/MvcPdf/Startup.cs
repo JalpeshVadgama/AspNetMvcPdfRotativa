adfb352b949ef59d288958734fd7e688bf401b00
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-02 11:14:52 +0530

    Initial Commit of ASP.NET MVC application.

---
+++ MvcPdf/MvcPdf/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPdf.Startup))]
+namespace MvcPdf
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
