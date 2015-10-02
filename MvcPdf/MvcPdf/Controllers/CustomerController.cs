53591f8176ac8ec11d49ec54aec8c6aaf769f930
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-02 11:35:02 +0530

    Added profile pic, Controller and View for the same.

---
+++ MvcPdf/MvcPdf/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using MvcPdf.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcPdf.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var imagePath = Server.MapPath("content/profile.jpg");
+
+            var customers = new List<Customer>()
+            {
+                new Customer {CustomerId = 1, FirstName = "Jalpesh", LastName = "Vadgama", ProfileImage = imagePath},
+                new Customer {CustomerId = 1, FirstName = "Vishal", LastName = "Vadgama", ProfileImage = imagePath}
+            };
+
+            return View(customers);
+
+        }
+    }
+}
