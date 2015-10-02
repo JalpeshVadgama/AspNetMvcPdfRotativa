274b7e5bdc059044dd04a4bb9f799a65afbe40b4
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-02 11:20:38 +0530

    Added Customer Model.

---
+++ MvcPdf/MvcPdf/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcPdf.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
