a0dfa5d5ba1dcfdc5a10400ca2a60781273ad7fb
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-30 00:15:13 +0530

    Removed unused names paces

--- MvcPdf/MvcPdf/Controllers/HomeController.cs
+++ MvcPdf/MvcPdf/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MvcPdf.Controllers
 {
@@ -11,6 +7,7 @@ namespace MvcPdf.Controllers
         public ActionResult Index()
         {
             return View();
+            
         }
 
         public ActionResult About()
